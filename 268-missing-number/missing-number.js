2a10a67d0545f143b9a0bef23d1122be0a79b99c
Md. Rashid Ansari <133353728+ansari21github@users.noreply.github.com>
2025-07-24 22:06:27 +0530

    Time: 0 ms (100.00%) | Memory: 55 MB (78.03%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -3,10 +3,19 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-  for(let i=0; i<=nums.length; i++){
-if(!nums.includes(i)){
-    return i;
-}
-  }  
-  return -1;
+
+    // Brute force solution o(n2)
+
+//   for(let i=0; i<=nums.length; i++){
+// if(!nums.includes(i)){
+//     return i;
+// }
+//   }  
+//   return -1;
+
+let n = nums.length;
+
+let sum = n * (n+1) / 2;
+let currentSum = nums.reduce((acc,crn)=>acc+crn,0)
+return sum - currentSum ;
 };
