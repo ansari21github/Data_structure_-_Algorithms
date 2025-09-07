cc0da211854c1aab7480b1fe6feb1ceac5e3904d
Md. Rashid Ansari <133353728+ansari21github@users.noreply.github.com>
2025-09-07 12:49:23 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (19.85%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/search-a-2d-matrix.js
@@ -4,10 +4,31 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    for(let i=0; i<matrix.length; i++){
-        if(matrix[i].includes(target)){
-            return true;
-        }
-    }
-    return false;
+    // for(let i=0; i<matrix.length; i++){
+    //     if(matrix[i].includes(target)){
+    //         return true;
+    //     }
+    // }
+    // return false;
+
+    //  O(m × n) 
+
+    // Optimal solution
+
+    let m = matrix.length; 
+    let n = matrix[0].length;
+    
+   let row = 0;
+   let col = n-1;
+
+   while(row < m && col>=0){
+
+    if(matrix[row][col] === target)return true;
+    else if(matrix[row][col] > target) col--;
+    else row++;
+   }
+   return false;
+
+//    Time Complexity: O(m + n)
+// Space Complexity: O(1)
 };
