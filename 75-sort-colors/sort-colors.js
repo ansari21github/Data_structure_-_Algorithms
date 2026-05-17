9b69bc90101a94a44daee46f94d3ad52b0cac4a9
Md. Rashid Ansari <133353728+ansari21github@users.noreply.github.com>
2026-05-17 23:23:57 +0530

    Time: 0 ms (100.00%) | Memory: 53.9 MB (53.43%) - LeetSync

---
+++ 75-sort-colors/sort-colors.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    let left = 0;
+    let mid = 0;
+    let right = nums.length-1;
+
+    while(mid <= right){
+        if(nums[mid] === 0){
+            let temp = nums[mid];
+            nums[mid] = nums[left];
+            nums[left] = temp;
+
+            left++;
+            mid++;
+        }else if(nums[mid] === 1){
+            mid++;
+        }else{
+            let temp = nums[mid];
+            nums[mid] = nums[right];
+            nums[right] = temp;
+            right--;
+        }
+    }
+    return nums
+};
