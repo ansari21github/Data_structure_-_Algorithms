a653de1d3ccf7e56ccca040fbf87ea3d7253a6e7
Md. Rashid Ansari <133353728+ansari21github@users.noreply.github.com>
2026-05-18 00:03:47 +0530

    Time: 1 ms (88.56%) | Memory: 57.1 MB (56.43%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,19 +3,36 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let obj = {};
-    let n = nums.length
-    for(let key of nums){
-        if(obj[key]){
-            obj[key]++;
-        }else{
-            obj[key] = 1;
-        }
-    }
+    // More voting algorithm
 
-    for(let key in obj){
-     if(obj[key] > (n/2)){
-        return Number(key);
-     }
+    // let obj = {};
+    // let n = nums.length
+    // for(let key of nums){
+    //     if(obj[key]){
+    //         obj[key]++;
+    //     }else{
+    //         obj[key] = 1;
+    //     }
+    // }
+
+    // for(let key in obj){
+    //  if(obj[key] > (n/2)){
+    //     return Number(key);
+    //  }
+    // }
+
+let el;
+let count = 0;
+
+for(let i=0; i<nums.length; i++){
+    if(count === 0){
+        el = nums[i];
+        count++;
+    }else if(nums[i] === el){
+        count++;
+    }else if(nums[i] != el){
+        count--;
     }
+}
+return el;
 };
