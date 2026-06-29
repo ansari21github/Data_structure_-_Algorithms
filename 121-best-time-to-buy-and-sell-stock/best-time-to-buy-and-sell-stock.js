60e02409af21d1619c6bd4a13a5c7f9a98e19bbd
Md. Rashid Ansari <133353728+ansari21github@users.noreply.github.com>
2026-06-29 15:57:36 +0530

    Time: 4 ms (31.88%) | Memory: 65.3 MB (51.79%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let mini = prices[0];
+    let max = 0;
+    let n = prices.length;
+
+    for(let i=1; i<n; i++){
+        let cost = prices[i] - mini;
+        max = Math.max(max , cost);
+        mini = Math.min(mini,prices[i]); 
+    }
+    return max;
+};
